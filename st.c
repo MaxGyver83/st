71a04f1e0dcdda80df1f1dcc393e9900df0a9ea7
Max Schillinger <maxschillinger@web.de>
2023-02-03 20:08:40 +0100

    No warning for unknown control sequences

--- st.c
+++ st.c
@@ -1637,6 +1637,8 @@ tsetmode(int priv, int set, const int *args, int narg)
 				      and can be mistaken for other control
 				      codes. */
 				break;
+			case 7727: /* tmux related!? Don't warn. */
+				break;
 			default:
 				fprintf(stderr,
 					"erresc: unknown private set/reset mode %d\n",
@@ -1678,8 +1680,8 @@ csihandle(void)
 	switch (csiescseq.mode[0]) {
 	default:
 	unknown:
-		fprintf(stderr, "erresc: unknown csi ");
-		csidump();
+		/* fprintf(stderr, "erresc: unknown csi "); */
+		/* csidump(); */
 		/* die(""); */
 		break;
 	case '@': /* ICH -- Insert <n> blank char */
