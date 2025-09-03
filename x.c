01e4c235e41595a5f0b9567f22d78526df4c7a24
Max Schillinger <maxschillinger@web.de>
2025-09-03 22:17:02 +0200

    Apply st-borderpx-option-20241008-e015463.diff

--- x.c
+++ x.c
@@ -2089,11 +2089,11 @@ run(void)
 void
 usage(void)
 {
-	die("usage: %s [-aiv] [-c class] [-f font] [-g geometry]"
+	die("usage: %s [-aiv] [-b borderpx] [-c class] [-f font] [-g geometry]"
 	    " [-n name] [-o file]\n"
 	    "          [-T title] [-t title] [-w windowid]"
 	    " [[-e] command [args ...]]\n"
-	    "       %s [-aiv] [-c class] [-f font] [-g geometry]"
+	    "       %s [-aiv] [-b borderpx] [-c class] [-f font] [-g geometry]"
 	    " [-n name] [-o file]\n"
 	    "          [-T title] [-t title] [-w windowid] -l line"
 	    " [stty_args ...]\n", argv0, argv0);
@@ -2161,6 +2161,9 @@ main(int argc, char *argv[])
 		break;
 	case 'A':
 		opt_alpha = EARGF(usage());
+		break;
+	case 'b':
+		borderpx = atoi(EARGF(usage()));
 		break;
 	case 'c':
 		opt_class = EARGF(usage());
